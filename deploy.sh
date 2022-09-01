40b56a3de443f42626af5fd98aa5c81c9af94ee3
JeminLim <wer0801@gmail.com>
2022-09-01 21:19:15 +0900

    modified build script

--- deploy.sh
+++ deploy.sh
@@ -23,40 +23,40 @@ then
   echo "> 현재 구동 중인 컨테이너가 없습니다."
 else
   echo "> 현재 구동 중인 컨테이너를 종료합니다. PID = ${CURRENT_PID} "
-  docker stop "${CURRENT_PID}"
+  sudo docker stop "${CURRENT_PID}"
   sleep 5
-  docker rm "${CURRENT_PID}"
+  sudo docker rm "${CURRENT_PID}"
   sleep 5
 fi
 
 echo "> 이미지 파일 빌드 ... "
 echo ">>> Config server 이미지 빌드"
-docker rmi ${DOCKER_USER_ID}/infra-config-server:${PREV_VERSION}
-docker build -f ${CONFIG_SERVER_DIR}/Dockerfile -t ${DOCKER_USER_ID}/infra-config-server:${APP_VERSION} .
+sudo docker rmi ${DOCKER_USER_ID}/infra-config-server:${PREV_VERSION}
+sudo docker build -f ${CONFIG_SERVER_DIR}/Dockerfile -t ${DOCKER_USER_ID}/infra-config-server:${APP_VERSION} .
 
 echo ">>> Eureka server 이미지 빌드"
-docker rmi ${DOCKER_USER_ID}/infra-eureka-server:${PREV_VERSION}
-docker build -f ${EUREKA_SERVER_DIR}/Dockerfile -t ${DOCKER_USER_ID}/infra-eureka-server:${APP_VERSION} .
+sudo docker rmi ${DOCKER_USER_ID}/infra-eureka-server:${PREV_VERSION}
+sudo docker build -f ${EUREKA_SERVER_DIR}/Dockerfile -t ${DOCKER_USER_ID}/infra-eureka-server:${APP_VERSION} .
 
 echo ">>> Api gateway 이미지 빌드"
-docker rmi ${DOCKER_USER_ID}/infra-api-gateway-servic:${PREV_VERSION}
-docker build -f ${API_GATEWAY_SERVICE_DIR}/Dockerfile -t ${DOCKER_USER_ID}/infra-api-gateway-servic:${APP_VERSION} .
+sudo docker rmi ${DOCKER_USER_ID}/infra-api-gateway-servic:${PREV_VERSION}
+sudo docker build -f ${API_GATEWAY_SERVICE_DIR}/Dockerfile -t ${DOCKER_USER_ID}/infra-api-gateway-servic:${APP_VERSION} .
 
 echo ">>> Member service 이미지 빌드"
-docker rmi ${DOCKER_USER_ID}/member-service:${PREV_VERSION}
-docker build -f ${MEMBER_SERVICE_DIR}/Dockerfile -t ${DOCKER_USER_ID}/member-service:${APP_VERSION} .
+sudo docker rmi ${DOCKER_USER_ID}/member-service:${PREV_VERSION}
+sudo docker build -f ${MEMBER_SERVICE_DIR}/Dockerfile -t ${DOCKER_USER_ID}/member-service:${APP_VERSION} .
 
 echo ">>> Member auth service 이미지 빌드"
-docker rmi ${DOCKER_USER_ID}/member-auth-service:${PREV_VERSION}
-docker build -f ${MEMBER_AUTH_SERVICE_DIR}/Dockerfile -t ${DOCKER_USER_ID}/member-auth-service:${APP_VERSION} .
+sudo docker rmi ${DOCKER_USER_ID}/member-auth-service:${PREV_VERSION}
+sudo docker build -f ${MEMBER_AUTH_SERVICE_DIR}/Dockerfile -t ${DOCKER_USER_ID}/member-auth-service:${APP_VERSION} .
 
 echo ">>> Mentoring service 이미지 빌드"
-docker rmi ${DOCKER_USER_ID}/mentoring-service:${PREV_VERSION}
-docker build -f ${MENTORING_SERVICE_DIR}/Dockerfile -t ${DOCKER_USER_ID}/mentoring-service:${APP_VERSION} .
+sudo docker rmi ${DOCKER_USER_ID}/mentoring-service:${PREV_VERSION}
+sudo docker build -f ${MENTORING_SERVICE_DIR}/Dockerfile -t ${DOCKER_USER_ID}/mentoring-service:${APP_VERSION} .
 
 echo ">>> Reply service 이미지 빌드"
-docker rmi ${DOCKER_USER_ID}/reply-service:${PREV_VERSION}
-docker build -f ${REPLY_SERVICE_DIR}/Dockerfile -t ${DOCKER_USER_ID}/reply-service:${APP_VERSION} .
+sudo docker rmi ${DOCKER_USER_ID}/reply-service:${PREV_VERSION}
+sudo docker build -f ${REPLY_SERVICE_DIR}/Dockerfile -t ${DOCKER_USER_ID}/reply-service:${APP_VERSION} .
 
 echo "> Docker-compose 구동"
 cd ~
